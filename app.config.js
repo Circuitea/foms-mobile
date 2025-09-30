f368310c31ff146afda2a62ec7a8178cc538f332
DJCAYZ <charlesaaron07@gmail.com>
2025-10-01 01:17:43 +0800

    redefine app package and name for defaults

--- app.config.js
+++ app.config.js
@@ -1,9 +1,15 @@
 
 export default ({ config }) => ({
   ...config,
+  name: process.env.APP_NAME ?? config.name,
   android: {
     ...config.android,
+    package: process.env.PACKAGE_NAME ?? config.android?.package,
     googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
+  },
+  ios: {
+    ...config.ios,
+    bundleIdentifier: process.env.PACKAGE_NAME ?? config.ios?.bundleIdentifier,
   },
   extra: {
     ...config.extra,
